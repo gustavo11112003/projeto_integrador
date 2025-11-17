61d55b31b9fe63f4408d5eec8347d606933326bd
gustavo11112003 <itbtkmisery@gmail.com>
2025-11-17 20:11:52 +0000

    FINAL

--- projeto/src/main/java/recursos/ProdutoRecurso.java
+++ projeto/src/main/java/recursos/ProdutoRecurso.java
@@ -5,9 +5,12 @@ import java.util.List;
 import entidades.Produto;
 import io.quarkus.panache.common.Sort;
 import jakarta.transaction.Transactional;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 
 @Path("produtos")
 public class ProdutoRecurso {
@@ -20,5 +23,33 @@ public class ProdutoRecurso {
     @Transactional
     public void salvar (Produto produto) {
         produto.persist();
+    }
+
+    @DELETE
+    @Path("{codigo}")
+    @Transactional
+    public void excluir (@PathParam("codigo") Integer codigo) {
+        Produto produto = Produto.findById(codigo);
+
+        if (produto != null) {
+            produto.delete();
+        }
+    }
+
+    @PUT
+    @Transactional
+    @Path("{codigo}")
+    public void editar (@PathParam("codigo") Integer codigo, Produto produto) {
+        Produto produtoExistente = Produto.findById(codigo);
+
+        if (produtoExistente != null) {
+            produtoExistente.nome = produto.nome;
+            produtoExistente.quantidade = produto.quantidade;
+            produtoExistente.preco = produto.preco;
+            produtoExistente.categoria = produto.categoria;
+            produtoExistente.fabricante = produto.fabricante;
+
+            produtoExistente.persist();
+        }
     }
 }
